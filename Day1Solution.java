339a1bcf0162546172e60ed69a9eb43c1e82ba34
Prahlad M Vijay <mvijayprahlad@gmail.com>
2020-08-19 08:13:09 +0530

    Update Day1Solution.java

--- Day1Solution.java
+++ Day1Solution.java
@@ -6,37 +6,40 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
-
-
+public class Day1Solution{
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         String[] nd = scanner.nextLine().split(" ");
-        int rot_index;
+
         int size = Integer.parseInt(nd[0]);
 
         int rot = Integer.parseInt(nd[1]);
 
-        int[] a = new int[size];
-        int[] rot_arr = new int[size];
+        int[] arr = new int[size];
 
         String[] aItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < size; i++) {
             int aItem = Integer.parseInt(aItems[i]);
-            a[i] = aItem;
+            arr[i] = aItem;
         }
 
-        for (int i = 0; i < size; i++) {
-            rot_index = (i + size - rot) % size;
-            rot_arr[rot_index] = a[i];
+        for(int count = 0; count < rot; count++) {
+            
+            int temp = arr[0];
+
+            for (int i = 0, j = 1; i < size && j < size; i++, j++) 
+                arr[i] = arr[j];            
+            
+            arr[size-1] = temp;
         }
 
         for(int i = 0; i< size; i++)    
-          System.out.print(rot_arr[i] + " ");
+          System.out.print(arr[i] + " ");
+        System.out.println();
         scanner.close();
     }
 }
